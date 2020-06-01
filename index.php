93eb58511709e0331a39ad8fca47567ed6f4b412
Paulo-Crisci <46006723+Paulo-Crisci@users.noreply.github.com>
2020-05-31 23:54:45 -0300

    MoneyPlant

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    
+</head>
+<body>
+    <div  class="middle">
+      <div class="menu">
+        <li class="item" id='profile'>
+          <a href="#profile" class="btn"><i class="far fa-user"></i>Usuarios</a>
+          <div class="smenu">
+            <a href="cadastraaluno.php">Cadastrar Usuário</a>
+           
+          </div>
+        </li>
+          <a class="btn" href="#"><i class="fas fa-sign-out-alt"></i>Sair</a>
+        </li>
+      </div>
+    </div>
+  </body>
+</html>
+
+
